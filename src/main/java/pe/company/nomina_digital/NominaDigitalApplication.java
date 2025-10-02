6e9096ba55c053ca18f5c20c13ffa93ca8cd0465
Omori-Q <fabianestrada716@gmail.com>
2025-10-01 19:49:48 -0500

    Creación del proyecto

---
+++ src/main/java/pe/company/nomina_digital/NominaDigitalApplication.java
@@ -0,0 +1,13 @@
+package pe.company.nomina_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NominaDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/company/nomina_digital/NominaDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.company.nomina_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
